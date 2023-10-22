927f9d4a31fbfe1e580580fcc207f91765df8962
zoning113 <giwoni05@naver.com>
2023-10-22 22:01:52 +0900

    hankyung

--- test1/spiders/crawling.py
+++ test1/spiders/crawling.py
@@ -14,34 +14,31 @@ class NewsCrawlingSpider(scrapy.Spider):
 
     # 뉴스 리스트 페이지에서 24시간 이내의 기사만 선별 후 해당 기사 페이지를 요청한다. (뉴스 리스트 페이지에서 'link', 'category' 'date' 데이터 파싱 후 callback)
     def parse_hankyung(self, response):
-        for sel in response.xpath('//*[@id="section-list"]/ul/li'):
+        for sel in response.xpath('//*[@id="container"]/div/div[1]/div[2]/div/div[2]/ul/li[1]'):
+            #//*[@id="container"]/div/div[1]/div[2]/div/div[2]/ul/li[1]
             news_date = parse(sel.xpath('.//div[@class="txt-cont"]/span/text()').extract()[0].strip())
             #//*[@id="container"]/div/div[1]/div[2]/div/div[2]/ul/li[1]/div/span
             if self.now - news_date < dt.timedelta(days=1):
                 item = NewsCrawlingItem()
                 item['site_source'] = sel.xpath('div[@class="txt-cont"]/h2/a/@href').extract()[0].strip()
                 #//*[@id="container"]/div/div[1]/div[2]/div/div[2]/ul/li[1]/div/h2/a
-                #삭제item['category'] = sel.xpath('div[@class="view-cont"]/span/em[1]/text()').extract()[0].strip()
-                #삭제item['date'] = sel.xpath('div[@class="view-cont"]/span/em[3]/text()').extract()[0].strip()
-                item['content_section'] = None
-                item['created_at'] = sel.xpath().extract()[0].strip()
-                #수정필요&xpath
+                item['content_section'] = 'Gen'
+                item['created_at'] = sel.xpath('.//div[@class="txt-cont"]/span/text()').extract()[0].strip()
+                #//*[@id="container"]/div/div[1]/div[2]/div/div[2]/ul/li[1]/div/span
                 item['site_image'] = sel.xpath('a/img/@src').extract()[0].strip()
                 #//*[@id="container"]/div/div[1]/div[2]/div/div[2]/ul/li[3]/div[2]/a/img
                 item['site_location'] = 'KR'
                 item['contents_type'] = 'news'
-                request = scrapy.Request(item['site_source'], callback=self.parse_hankyung)
+                request = scrapy.Request(item['site_source'], callback=self.parse_hankyung2)
                 request.meta['item'] = item
                 yield request
 
     # 24시간 이내의 기사들의 본문과 타이틀을 파싱한다.
     def parse_hankyung2(self, response):
         item = response.meta['item']
-        item['site_subject'] = response.xpath('//*[@class="txt-cont"]/h2/a/text()').extract()[0].strip()
+        item['site_subject'] = response.xpath('//*[@class="article container v2"]/h1/text()').extract()
+        #//*[@id="container"]/div/div/article/h1
+        item['site_content'] = response.xpath('//*[@id="article-body]/div/text()').extract()
+        #//*[@id="articletxt"]
         #//*[@id="container"]/div/div[1]/div[2]/div/div[2]/ul/li[2]/div/h2/a
-        # item['image_url'] = response.xpath('//*[@id="article-view-content-div"]/div[2]/figure/div/img/@src').extract()[0].strip()
-        for sel in response.xpath('//*[@class=""]'):
-        #수정필요
-        #//*[@id="container"]/div/div[1]/div[2]/div/div[2]/ul/li[2]/div/h2/a
-            item['site_content'] = sel.xpath('p/text()').extract()
         yield item
